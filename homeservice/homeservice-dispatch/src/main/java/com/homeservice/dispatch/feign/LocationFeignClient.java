4de75f381703c8443ba1350ad73300e1456a6a24
ByATore <2233861327@qq.com>
2026-06-16 09:33:40 +0800

    fix: LocationFeignClient 缺少 PathVariable import

--- homeservice/homeservice-dispatch/src/main/java/com/homeservice/dispatch/feign/LocationFeignClient.java
+++ homeservice/homeservice-dispatch/src/main/java/com/homeservice/dispatch/feign/LocationFeignClient.java
@@ -3,6 +3,7 @@ package com.homeservice.dispatch.feign;
 import com.homeservice.common.result.Result;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
